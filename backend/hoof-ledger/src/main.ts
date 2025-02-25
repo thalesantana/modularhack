b7f7d16c7214c0030e3c3e05adc04cece2c9830a
Renato Gravino Neto <renato.gravino@skallardigital.com.br>
2025-02-25 20:45:03 -0300

    feat: ajustando mongodb

--- backend/hoof-ledger/src/main.ts
+++ backend/hoof-ledger/src/main.ts
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.getHttpAdapter().getInstance().disable('x-powered-by');
+
+  Logger.log(`Servidor rodando na porta ${port}`, 'main.ts');
+  await app.listen(port);
 }
-bootstrap();
+void bootstrap();
