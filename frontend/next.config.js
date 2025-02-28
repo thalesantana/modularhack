1e10d5955098728c0f7216241eb51ae9fb958476
Thales Santana <thales.c.santana@outlook.com>
2025-02-28 03:42:29 -0300

    feat: remove Babel configuration, update Next.js config, and add new dependencies

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'out',  // This specifies the output directory name
   eslint: {
     ignoreDuringBuilds: true,
   },
